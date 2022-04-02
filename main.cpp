a6b87802c7b5797bd121c4d83874bf7b95549938
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2022-04-02 17:44:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float x,y,a,b;
+    cin >> a >> b >> x >> y ;
+    if(x<0 && y>0){
+    cout << a*x-b*y << endl;
+    }else if(x>=0 && y<=0){
+        cout << a * x*x + b * y*x << endl;
+    }else{
+        cout << a*x+b*y*y << endl;
+    }
+    return 0;
 }
