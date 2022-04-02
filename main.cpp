82494d2cff367f4b9c04c7b06803a4b09ae9bb8f
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2022-04-02 17:45:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     if(x<0 && y>0){
     cout << a*x-b*y << endl;
     }else if(x>=0 && y<=0){
-        cout << a * x*x + b * y*x << endl;
+        cout << a*x*x - b * y*x << endl;
     }else{
         cout << a*x+b*y*y << endl;
     }
